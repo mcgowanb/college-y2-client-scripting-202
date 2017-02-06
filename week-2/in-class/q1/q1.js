1f06bc5cdf97f4e4477b0a74decdeb5e2ddecbb9
Brian McGowan <mcgowan.b@gmail.com>
2017-02-06 14:51:18 +0000

    class exercises

--- week-2/in-class/q1/q1.js
+++ week-2/in-class/q1/q1.js
@@ -1,31 +1,38 @@
-(function(){
-   document.getElementById('store').addEventListener("click", saveDetails, false);
-   document.getElementById('clear').addEventListener("click", clearDetails, false);
-   document.getElementById('get').addEventListener("click", getDetails, false);
+(function () {
+    document.getElementById('store').addEventListener("click", saveDetails, false);
+    document.getElementById('clear').addEventListener("click", clearDetails, false);
+    document.getElementById('get').addEventListener("click", getDetails, false);
 })();
 
-function StudentDetails(name, course, Sid) {
+function studentDetails(name, course, Sid) {
     this.name = name;
     this.course = course;
     this.Sid = Sid
 }
 
-function clearDetails(){
+function clearDetails() {
     localStorage.clear();
 }
 
-function saveDetails(){
-    var name = document.getElementById('name');
-    var id = document.getElementById('std-id');
-    var course = document.getElementById('course');
-    var data = new StudentDetails(name, id, course);
+function saveDetails() {
+    var name = document.getElementById('name').value;
+    var id = document.getElementById('std-id').value;
+    var course = document.getElementById('course').value;
+    var data = new studentDetails(name, course, id);
     saveDetailsToStorage(data);
 }
 
-function saveDetailsToStorage(data){
-    localStorage.setItem(data.Sid, data);
+function saveDetailsToStorage(data) {
+    localStorage.setItem(data.Sid, JSON.stringify(data));
 }
 
-function getDetails(){
-    var l = localStorage.length;
+function getDetails() {
+    for (var i = 0; i < localStorage.length; i++) {
+        var item = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        var message = "==========PRINTING STUDENT DETAILS==========\n";
+        message += "Student Name: " + item.name + "\n";
+        message += "Student ID: " + item.Sid + "\n";
+        message += "Student Course: " + item.course + "\n\n";
+        document.getElementById('output').innerText += message;
+    }
 }
