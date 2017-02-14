372154010906314384e979cc4969dce72634a404
Brian McGowan <mcgowan.b@gmail.com>
2017-02-14 13:35:09 +0000

    ca-1 q1 storing book and image complete

--- ca-1/q1/q1.js
+++ ca-1/q1/q1.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         e.preventDefault();
         addBook();
         displayMessage("Book added to storage");
-        // clearfields();
+        clearfields();
     });
 
     $("#clear-list").on("click", function (e) {
@@ -13,6 +13,8 @@ $(document).ready(function () {
         displayMessage("Storage has been emptied.")
     });
 
+
+    //drop zone for image
     var dz = document.getElementById('book-cover');
 
     dz.addEventListener('dragover', function (e) {
@@ -39,7 +41,12 @@ function addBook() {
     var isbn = $("#isbn").val();
     var publisher = $("#publisher").val();
     var price = $("#price").val();
-    var image = bookCoverImage.src; //src
+    var image = "";
+
+    //null check in case the book cover is not added
+    if(bookCoverImage != null){
+    image = bookCoverImage.src; //src
+    }
 
     var book = new Book(name, author, isbn, publisher, price, image);
     localStorage.setItem(isbn, JSON.stringify(book));
@@ -47,10 +54,17 @@ function addBook() {
 
 
 function clearfields() {
-    $("#book-name, #author, #isbn, #publisher, #category, #book-cover").val("");
+    $("#book-name, #author, #isbn, #publisher, #price").val("");
+
+    // var elem = document.createElement('p').setAttribute("class", "text-center");
+    // console.log(elem);
+    // elem.text = "Drop book cover here";
+    $("#book-cover").children("img:first").remove();
+    $("#book-cover").append("<p class='text-center'>Drop book cover here</p>");
     //book cover remove child
 }
 
+//simple display message that flashes in and out
 function displayMessage(message) {
     $("#info-message").html(message).fadeIn("slow").delay(1000).fadeOut("slow");
 }
