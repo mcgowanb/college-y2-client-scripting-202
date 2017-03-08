d63f43ef551a0891a05bb20a26fef956118c3e3a
Brian McGowan <mcgowan.b@gmail.com>
2017-03-08 16:52:15 +0000

    classwork completed

--- week-7/algenius/script.js
+++ week-7/algenius/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function (){
 
+    $("ul li").each(function(k, v){
+        //key values of index and element. logging for display
+       // console.log(v);
+    });
+
+    $res = $(".row0").find(".fa-envelope-o").parent().text();
+    console.log($res);
+
+
+    $other = $(this).find("li.active");
+    $other.each(function(k, v){
+        console.log(v);
+    });
+
     $("a").on("click", function(e){e.preventDefault()});
 
     $(".nospace.inline li:first").one("click", function(){
@@ -26,7 +40,19 @@ $(document).ready(function (){
         var $parent = $(this).parent();
         $(this).remove();
         var $img = $('<img id="dynamic" width="100px"; height="100px">');
+        $img.attr("src", "images/image.gif");
+        $img.css({"border": "3px dashed green"});
         $parent.append($img);
     });
+
+    $("#pageintro > h3:first").hover(function(){
+        $(this).css({"font-size": "100px", "color": "green"}).attr("rel", "myNewRelTag").html("Here's some new html");
+       console.log("asfa");
+    });
+
+    $("#home-icon > i").on("click", function(){
+       $(this).addClass("myNewClass").removeClass("fa-lg").toggleClass("fa-home")
+    });
+
 
 });
